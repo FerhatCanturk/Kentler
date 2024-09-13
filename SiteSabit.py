231de264ceb0320ab3e340925a723b8a714fb32c
FerhatCanturk <122700408+FerhatCanturk@users.noreply.github.com>
2024-09-13 11:02:40 +0300

    SiteSabit.py

--- SiteSabit.py
+++ SiteSabit.py
@@ -3,17 +3,17 @@ import SQLServer
 def Sabitler():
     SiteSabitleri = SQLServer.Sorgula("SELECT * FROM SKWebSabitler")
     Sabitler={
-    'SiteSahibi': SiteSabitleri[0]["Sutun01"].strip(),
-    'Unvan':      SiteSabitleri[0]["Sutun02"].strip(),
-    'Slogan':     SiteSabitleri[0]["Sutun03"].strip(),
-    'AdminMail':  SiteSabitleri[0]["Sutun08"].strip(),
-    'Adres':      SiteSabitleri[0]["Sutun09"].strip(),
-    'Telefon':    SiteSabitleri[0]["Sutun10"].strip(),
-    'BankaSube':  SiteSabitleri[0]["Sutun11"].strip(),
-    'IBANNo':     SiteSabitleri[0]["Sutun12"].strip(),
-    'PassWord':   SiteSabitleri[0]["Sutun14"].strip(),
-    'SmtpHost':   SiteSabitleri[0]["Sutun15"].strip(),
-    'Port':       SiteSabitleri[0]["Sutun16"].strip(),
-    'MsgSys':     str(SiteSabitleri[0]["Sutun13"])
+    'SiteSahibi': SiteSabitleri[0].get("Sutun01", "").strip(),
+    'Unvan':      SiteSabitleri[0].get("Sutun02","").strip(),
+    'Slogan':     SiteSabitleri[0].get("Sutun03","").strip(),
+    'AdminMail':  SiteSabitleri[0].get("Sutun08","").strip(),
+    'Adres':      SiteSabitleri[0].get("Sutun09","").strip(),
+    'Telefon':    SiteSabitleri[0].get("Sutun10","").strip(),
+    'BankaSube':  SiteSabitleri[0].get("Sutun11","").strip(),
+    'IBANNo':     SiteSabitleri[0].get("Sutun12","").strip(),
+    'PassWord':   SiteSabitleri[0].get("Sutun14","").strip(),
+    'SmtpHost':   SiteSabitleri[0].get("Sutun15","").strip(),
+    'Port':       SiteSabitleri[0].get("Sutun16","").strip(),
+    'MsgSys':     str(SiteSabitleri[0].get("Sutun13"),"")
     }
     return Sabitler
