a1c7a954c1742f6b15be9e28628f6653ccbbdc2a
FerhatCanturk <122700408+FerhatCanturk@users.noreply.github.com>
2024-09-13 10:35:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import time, os, SQLServer, SiteSabit
 from flask import Flask, session
 ###############################################################################################################    
-from views                        import pyIndex
+from views                                import pyIndex
 from views.pyIndexKK                      import IndexKK
 from views.pyIndexSosyal                  import IndexSosyal
 from views.pyIndexSatis                   import IndexSatis
